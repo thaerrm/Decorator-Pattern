8be5804ca3c42711b421319255f417ed1dc6e021
thaerrm <33045802+thaerrm@users.noreply.github.com>
2020-04-20 14:45:19 -0500

    Decorate

--- src/PizzaDecorator.java
+++ src/PizzaDecorator.java
@@ -1,2 +1,13 @@
-public class PizzaDecorator {
+abstract class PizzaDecorator implements Pizza
+{
+    protected Pizza pizza;
+    public PizzaDecorator( Pizza pizza )
+    {
+        this.pizza = pizza;
+    }
+
+    public String makePizza()
+    {
+        return pizza.makePizza();
+    }
 }
