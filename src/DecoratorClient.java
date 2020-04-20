e73d6dc67aa35d3d5c4688596cb64aca67299694
thaerrm <33045802+thaerrm@users.noreply.github.com>
2020-04-20 17:10:00 -0500

    making pizza

---
+++ src/DecoratorClient.java
@@ -0,0 +1,17 @@
+public class DecoratorClient
+{
+
+    public static void main( String args[] ) {
+        PlainPizza plainPizzaObj = new PlainPizza();
+        String plainPizza = plainPizzaObj.makePizza();
+        System.out.println(plainPizza);
+
+
+
+
+
+
+
+
+
+    }
