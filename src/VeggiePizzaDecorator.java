937eed42522a49f9ae8815f0a0ab45e4d9830388
thaerrm <33045802+thaerrm@users.noreply.github.com>
2020-04-24 00:03:26 -0500

    Veggie Pizza Decorator

--- src/VeggiePizzaDecorator.java
+++ src/VeggiePizzaDecorator.java
@@ -1,5 +1,18 @@
 public class VeggiePizzaDecorator {
 
     protected VeggiePizzaDecorator pizza;
+    {
+        public VeggiePizzaDecorator(Pizza pizza)
+        {
+            this.pizza = pizza;
+        }
+
+        public String makeVeggiePizza()
+
+        {
+
+            return pizza.makeVeggiePizza();
+    }
+
 
 }
