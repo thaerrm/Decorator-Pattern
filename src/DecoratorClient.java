d12eb67fa9a0ccf486a5976b37572632243f5578
thaerrm <33045802+thaerrm@users.noreply.github.com>
2020-04-25 22:38:52 -0500

    UML File

--- src/DecoratorClient.java
+++ src/DecoratorClient.java
@@ -3,7 +3,7 @@ public class DecoratorClient
 
     public static void main(String[] args) {
 
-        PlainPizza plainPizzaObj = new PlainPizza();
+       m PlainPizza plainPizzaObj = new PlainPizza();
 
         String plainPizza = plainPizzaObj.makePizza();
         System.out.println(plainPizza);
